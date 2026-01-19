886501a487c633619ebee0134e076ddfa5c5b364
Vespertili0 <s.jeschke@griffith.edu.au>
2026-01-19 13:57:27 +1100

    feat: Update ASX stock lists and add ASX200 index

--- pyquantflow/collection/asx.py
+++ pyquantflow/collection/asx.py
@@ -1,25 +1,63 @@
+# last updated: 19 January 2026
+# https://www.marketindex.com.au/
+
 asx20 = [
-    "ALL.AX", "ANZ.AX", "BHP.AX", "BXB.AX", "CBA.AX", "COL.AX", "CSL.AX", "FMG.AX", "GMG.AX", "MQG.AX",
-    "NAB.AX", "QBE.AX", "REA.AX", "RIO.AX", "TCL.AX", "WBC.AX", "WDS.AX", "WES.AX", "WOW.AX", "WTC.AX"
+    "ALL.AX", "ANZ.AX", "BHP.AX", "BXB.AX", "CBA.AX",
+    "CSL.AX", "FMG.AX", "GMG.AX", "MQG.AX", "NAB.AX",
+    "NST.AX", "QBE.AX", "RIO.AX", "SIG.AX", "TCL.AX",
+    "TLS.AX", "WBC.AX", "WDS.AX", "WES.AX", "WOW.AX"
 ]
+
 
 asx50 = [
-    "ANZ.AX", "APA.AX", "ALL.AX", "ASX.AX", "AIA.AX", "BHP.AX", "BXB.AX", "CAR.AX", "COH.AX", "COL.AX",
-    "CBA.AX", "CPU.AX", "CSL.AX", "EVN.AX", "FPH.AX", "FMG.AX", "GMG.AX", "IAG.AX", "JHX.AX", "JBH.AX",
-    "MQG.AX", "MPL.AX", "NAB.AX", "NST.AX", "ORG.AX", "PME.AX", "QAN.AX", "QBE.AX", "REA.AX", "RMD.AX",
-    "RIO.AX", "STO.AX", "SCG.AX", "SGH.AX", "SIG.AX", "SHL.AX", "S32.AX", "SGP.AX", "SUN.AX", "TLS.AX",
-    "TLC.AX", "TCL.AX", "VCX.AX", "SOL.AX", "WES.AX", "WBC.AX", "WTC.AX", "WDS.AX", "WOW.AX", "XRO.AX"
+    "AIA.AX", "ALQ.AX", "ALL.AX", "ANZ.AX", "BHP.AX", "BSL.AX", "BXB.AX", "CBA.AX", "COH.AX", "COL.AX",
+    "CPU.AX", "CSL.AX", "EVN.AX", "FPH.AX", "FMG.AX", "GMG.AX", "IAG.AX", "JHX.AX", "LNW.AX", "LYC.AX",
+    "MQG.AX", "MPL.AX", "NAB.AX", "NEM.AX", "NST.AX", "ORI.AX", "ORG.AX", "PLS.AX", "PME.AX", "QAN.AX",
+    "QBE.AX", "REA.AX", "RMD.AX", "RIO.AX", "SCG.AX", "SGH.AX", "SIG.AX", "S32.AX", "SGP.AX", "SUN.AX",
+    "TCL.AX", "TLS.AX", "SOL.AX", "WES.AX", "WBC.AX", "WTC.AX", "WDS.AX", "WOW.AX", "XRO.AX", "YAL.AX"
 ]
 
+
 asx100 = [
-    "AGL.AX", "ALQ.AX", "AMC.AX", "ALD.AX", "ANZ.AX", "APA.AX", "ARG.AX", "ALL.AX", "ASX.AX", "ALX.AX",
-    "AIA.AX", "AZJ.AX", "AFI.AX", "BOQ.AX", "BEN.AX", "BHP.AX", "XYZ.AX", "BSL.AX", "BXB.AX", "CMM.AX",
-    "CAR.AX", "CGF.AX", "CHC.AX", "CWY.AX", "COH.AX", "COL.AX", "CBA.AX", "CPU.AX", "CSL.AX", "DEG.AX", 
-    "DXS.AX", "APE.AX", "EBO.AX", "EDV.AX", "EVN.AX", "FPH.AX", "FMG.AX", "GMD.AX", "GMG.AX", "GPT.AX",
-    "GQG.AX", "HVN.AX", "HUB.AX", "IFT.AX", "IAG.AX", "JHX.AX", "JBH.AX", "LYC.AX", "MQG.AX", "MPL.AX", 
-    "MCY.AX", "MEZ.AX", "MGR.AX", "NAB.AX", "NWL.AX", "NEM.AX", "NXT.AX", "NST.AX", "ORI.AX", "ORG.AX",
-    "PRU.AX", "PLS.AX", "PME.AX", "QAN.AX", "QBE.AX", "QUB.AX", "RHC.AX", "REA.AX", "REH.AX", "RMD.AX", 
-    "RIO.AX", "SFR.AX", "STO.AX", "SCG.AX", "SEK.AX", "SGH.AX", "SIG.AX", "SHL.AX", "S32.AX", "SDF.AX",
-    "SGP.AX", "SUN.AX", "TNE.AX", "TLX.AX", "TLS.AX", "A2M.AX", "TLC.AX", "TPG.AX", "TCL.AX", "TWE.AX", 
-    "VCX.AX", "SOL.AX", "WES.AX", "WBC.AX", "WTC.AX", "WDS.AX", "WOW.AX", "WOR.AX", "XRO.AX", "YAL.AX"
+    "A2M.AX", "AIA.AX", "ALD.AX", "ALQ.AX", "ALX.AX", "ALL.AX", "AMCDD.AX", "ANZ.AX", "APA.AX", "APE.AX",
+    "ARG.AX", "ASX.AX", "AZJ.AX", "AFI.AX", "BEN.AX", "BHP.AX", "BSL.AX", "BXB.AX", "CAR.AX", "CBA.AX",
+    "CDA.AX", "CHC.AX", "CGF.AX", "CMM.AX", "COH.AX", "COL.AX", "CPU.AX", "CSL.AX", "DXS.AX", "EDV.AX",
+    "EVN.AX", "FPH.AX", "FMG.AX", "GMD.AX", "GMG.AX", "GPT.AX", "GGP.AX", "HVN.AX", "HUB.AX", "IGO.AX",
+    "IFT.AX", "IAG.AX", "JHX.AX", "JBH.AX", "LNW.AX", "LTR.AX", "LYC.AX", "MQG.AX", "MPL.AX", "MCY.AX",
+    "MEZ.AX", "MIN.AX", "MGR.AX", "NAB.AX", "NWL.AX", "NEM.AX", "NXT.AX", "NST.AX", "ORI.AX", "ORG.AX",
+    "PRU.AX", "PLS.AX", "PME.AX", "QAN.AX", "QBE.AX", "QUB.AX", "RMS.AX", "RHC.AX", "REA.AX", "REH.AX",
+    "RMD.AX", "RIO.AX", "SFR.AX", "STO.AX", "SCG.AX", "SEK.AX", "SGH.AX", "SIG.AX", "SHL.AX", "S32.AX",
+    "SGP.AX", "SUN.AX", "TNE.AX", "TLS.AX", "TLC.AX", "TPG.AX", "TCL.AX", "VAU.AX", "VCX.AX", "SOL.AX",
+    "WES.AX", "WGX.AX", "WBC.AX", "WHC.AX", "WTC.AX", "WDS.AX", "WOW.AX", "WOR.AX", "XRO.AX", "YAL.AX"
+]
+
+
+
+asx200 = [
+    "360.AX", "4DX.AX", "AAI.AX", "A2M.AX", "AFI.AX", "AGL.AX", "AIA.AX", "ALK.AX",
+    "ALD.AX", "ALQ.AX", "ALX.AX", "AMCDD.AX", "AMP.AX", "ANN.AX", "ANZ.AX", "APA.AX",
+    "APE.AX", "ARB.AX", "ARG.AX", "ASB.AX", "ASX.AX", "AUB.AX", "AZJ.AX", "BAP.AX",
+    "BEN.AX", "BFL.AX", "BGL.AX", "BHP.AX", "BOQ.AX", "BPT.AX", "BRG.AX", "BSL.AX",
+    "BXB.AX", "BWP.AX", "CAR.AX", "CBA.AX", "CDA.AX", "CEN.AX", "CGF.AX", "CHC.AX",
+    "CIA.AX", "CIP.AX", "CLW.AX", "CMM.AX", "CNU.AX", "COL.AX", "CPU.AX", "CQR.AX",
+    "CSC.AX", "CSL.AX", "CTD.AX", "CWY.AX", "CYL.AX", "DBI.AX", "DMP.AX", "DNL.AX",
+    "DOW.AX", "DRO.AX", "DRR.AX", "DXS.AX", "DYL.AX", "EBO.AX", "EDV.AX", "EMR.AX",
+    "EVN.AX", "EVT.AX", "FBU.AX", "FLT.AX", "FMG.AX", "FPH.AX", "FRW.AX", "GDG.AX",
+    "GGP.AX", "GMD.AX", "GMG.AX", "GNE.AX", "GPT.AX", "GQG.AX", "GYG.AX", "HDN.AX",
+    "HMC.AX", "HUB.AX", "HVN.AX", "IAG.AX", "IFT.AX", "IGO.AX", "IFL.AX", "ILU.AX",
+    "INA.AX", "IPX.AX", "IRE.AX", "JDO.AX", "JBH.AX", "JHX.AX", "L1G.AX", "LLC.AX",
+    "LNW.AX", "LOV.AX", "LSF.AX", "LTR.AX", "LYC.AX", "MAF.AX", "MAQ.AX", "MCY.AX",
+    "MEZ.AX", "MFF.AX", "MIN.AX", "MGR.AX", "MND.AX", "MQG.AX", "MP1.AX", "MPL.AX",
+    "MSB.AX", "MTS.AX", "MXT.AX", "NAB.AX", "NCK.AX", "NEC.AX", "NEU.AX", "NIC.AX",
+    "NHC.AX", "NEM.AX", "NHF.AX", "NMT.AX", "NSR.AX", "NST.AX", "NWL.AX", "NWH.AX",
+    "NXT.AX", "OBM.AX", "ORA.AX", "ORG.AX", "ORI.AX", "PDN.AX", "PDI.AX", "PGR.AX",
+    "PLS.AX", "PME.AX", "PMV.AX", "PNI.AX", "PPT.AX", "PRN.AX", "PRU.AX", "PXA.AX",
+    "QAN.AX", "QBE.AX", "QUB.AX", "REA.AX", "REG.AX", "REH.AX", "RGN.AX", "RHC.AX",
+    "RIO.AX", "RMD.AX", "RMS.AX", "RRL.AX", "RSG.AX", "RWC.AX", "RYM.AX", "SCG.AX",
+    "SDF.AX", "SEK.AX", "SFR.AX", "SGH.AX", "SGM.AX", "SGP.AX", "SHL.AX", "SIG.AX",
+    "SMR.AX", "SNZ.AX", "SOL.AX", "SPK.AX", "S32.AX", "STO.AX", "SUN.AX", "SUL.AX",
+    "TAH.AX", "TCL.AX", "TLC.AX", "TLS.AX", "TLX.AX", "TNE.AX", "TPG.AX", "TUA.AX",
+    "TWE.AX", "VAU.AX", "VCX.AX", "VEA.AX", "VGN.AX", "VNT.AX", "VUL.AX", "WAF.AX",
+    "WAM.AX", "WBC.AX", "WDS.AX", "WEB.AX", "WES.AX", "WGX.AX", "WHC.AX", "WOW.AX",
+    "WOR.AX", "WPR.AX", "WTC.AX", "XRO.AX", "YAL.AX", "ZIM.AX", "ZIP.AX"
 ]
