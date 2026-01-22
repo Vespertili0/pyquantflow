0d4a536b2a1a2fae5989a9805cb11167f29b8be0
Vespertili0 <s.jeschke@griffith.edu.au>
2026-01-22 15:04:27 +1100

    fix: Update requirements and setup files to include missing dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ with open("requirements.txt") as f:
 setup(
     name="pyquantflow",
     version="0.2.0",
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
     description="A robust stock analysis and backtesting framework.",
     packages=find_packages(include=["pyquantflow", "pyquantflow.*"]),
     install_requires=required,
